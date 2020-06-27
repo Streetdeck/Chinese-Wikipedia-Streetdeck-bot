9604a0291f35d61b5fe3c47907a859db85250863
Streeetdeck <57138442+Streetdeck@users.noreply.github.com>
2020-06-27 21:30:35 +0800

    Update edit.py

--- rx-archive/edit.py
+++ rx-archive/edit.py
@@ -1,5 +1,10 @@
 from datetime import datetime
-from config import config_page_name
+
+try:
+    from config import config_page_name
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("Please copy config.sample.py as config.py, setting up, and try again.")
+
 import json
 import os
 import re
@@ -7,6 +12,7 @@ import time
 import mwparserfromhell
 import pywikibot
 
+defaultTime = datetime(1, 1, 1)
 os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
 
 site = pywikibot.Site()
@@ -18,7 +24,7 @@ cfg = json.loads(cfg)
 print(json.dumps(cfg, indent=4, ensure_ascii=False))
 
 if not cfg["enable"]:
-    exit("disabled\n")
+    exit("disabled")
 
 rxpage = pywikibot.Page(site, cfg["main_page_name"])
 text = rxpage.text
@@ -28,59 +34,50 @@ wikicode = mwparserfromhell.parse(text)
 archivelist = {}
 count = 0
 
- for template in section.filter_templates():
-        if template.name.lower() == "status2":
-            if template.has(1):
-                status = template.get(1)
-            else:
-                status = "(empty)"
-            print("status", status, end="\n")
-            if status in cfg["publicizing_status"]:
-                publicizing = True
-                print("publicizing", end="\n")
-                break
-            elif status in cfg["done_status"]:
-                processed = True
-                print("procesed", end="\n")
-                break
-                
-                 lasttime = datetime(1, 1, 1)
-    for m in re.findall(r"(\d{4})年(\d{1,2})月(\d{1,2})日 \(.\) (\d{2}):(\d{2}) \(UTC\)", str(section)):
-        d = datetime(int(m[0]), int(m[1]), int(m[2]), int(m[3]), int(m[4]))
-        lasttime = max(lasttime, d)
-    print(lasttime, end="\n)"
-          
-      processed = False
-    if re.search(cfg["processed_regex"], str(section)) and not re.search(cfg["not_processed_regex"], str(section)):
-        processed = True
-        print("processed", end="\n")
-    else:
-        print("not processed", end="\n")
+for template in wikicode.filter_templates():
+    if template.name.lower() == "status2":
+        if template.has(1):
+            status = template.get(1)
+        else:
+            status = "(empty)"
+        print(f"Status: {status}")
+        if status in cfg["publicizing_status"]:
+            publicizing = True
+            print("Publicizing...")
+            break
+        elif status in cfg["done_status"]:
+            processed = True
+            print("Processed.")
+            break
 
-    if (
-        (
-            (processed and time.time() - lasttime.timestamp() > cfg["time_to_live_for_processed"])
-            or (not processed and time.time() - lasttime.timestamp() > cfg["time_to_live_for_not_processed"])
-        )
-            and lasttime != datetime(1, 1, 1)):
-        target = (lasttime.year, lasttime.month)
-         if (
-        (
-            (processed and not publicizing and time.time() - lasttime.timestamp() > cfg["time_to_live_for_processed"])
-            or (not processed and not publicizing and time.time() - lasttime.timestamp() > cfg["time_to_live_for_not_processed"])
-        )
-            and lasttime != datetime(1, 1, 1)):
-        target = (lasttime.year, lasttime.month)
-        if target not in archivelist:
-            archivelist[target] = []
-        archivestr = str(section).strip()
-        archivestr = re.sub(
-            r"{{bot-directive-archiver\|no-archive-begin}}[\s\S]+?{{bot-directive-archiver\|no-archive-end}}\n?", "", archivestr)
-        archivelist[target].append(archivestr)
-        count += 1
-        section.remove(section)
-        print("archive to " + str(target), end="\n")
-    print()
+lasttime = datetime(1, 1, 1)
+for m in re.findall(r"(\d{4})年(\d{1,2})月(\d{1,2})日 \(.\) (\d{2}):(\d{2}) \(UTC\)", str(wikicode)):
+    d = datetime(int(m[0]), int(m[1]), int(m[2]), int(m[3]), int(m[4]))
+    lasttime = max(lasttime, d)
+print(f"var: lasttime: {lasttime}")
+
+processed = False
+if re.search(cfg["processed_regex"], str(wikicode)) and not re.search(cfg["not_processed_regex"], str(wikicode)):
+    processed = True
+    print("Processed.")
+else:
+    print("Not processed.")
+
+t1 = time.time() - lasttime.timestamp()
+if (processed and t1 > cfg["time_to_live_for_processed"]) or (not processed and t1 > cfg["time_to_live_for_not_processed"]) and (lasttime != datetime(1, 1, 1)):
+    target = (lasttime.year, lasttime.month)
+
+if ((processed and not publicizing and t1 > cfg["time_to_live_for_processed"]) or (not processed and not publicizing and t1 > cfg["time_to_live_for_not_processed"])) and lasttime != datetime(1, 1, 1):
+    target = (lasttime.year, lasttime.month)
+    if target not in archivelist:
+        archivelist[target] = []
+    archivestr = str(wikicode).strip()
+    archivestr = re.sub(
+        r"{{bot-directive-archiver\|no-archive-begin}}[\s\S]+?{{bot-directive-archiver\|no-archive-end}}\n?", "", archivestr)
+    archivelist[target].append(archivestr)
+    count += 1
+    wikicode.remove(wikicode)
+    print("Archive to " + str(target))
     
 pywikibot.showDiff(rxpage.text, text)
 rxpage.text = text
